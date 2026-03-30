b0afff3be29b3c0639d4c4dd765d32c64fcb9dbe
ShihabRafi1205 <rafy191205@gmail.com>
2026-03-30 11:28:39 +0200

    created navbar and banner section

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import BannerImg from "../../assets/banner.png";
+import { IoPlayOutline } from "react-icons/io5";
+
+const Banner = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen justify-around">
+      <div className="hero-content flex gap-20 flex-col lg:flex-row-reverse">
+        <img src={BannerImg} className="max-w-sm rounded-lg w-full" />
+        <div>
+          <h1 className="text-5xl font-bold">
+            Supercharge Your <br /> Digital Workflow
+          </h1>
+          <p className="py-6">
+            Access premium AI tools, design assets, templates, and productivity{" "}
+            <br />
+            software—all in one place. Start creating faster today. <br />{" "}
+            Explore Products
+          </p>
+          <div className="flex gap-4">
+            {" "}
+            <button className="btn btn-primary rounded-full">
+              Explore Products
+            </button>
+            <button className="btn border border-blue-600 rounded-full text-blue-600">
+              <IoPlayOutline /> Watch Demo
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { CiShoppingCart } from "react-icons/ci";
+
+
+const Navbar = () => {
+  return (
+    <div className="navbar lg:px-12 bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+        </div>
+        <a className="font-bold bg-linear-to-r from-blue-500 to-purple-500 text-2xl bg-clip-text text-transparent">
+          DigiTools
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="flex gap-4 menu menu-horizontal px-1">
+          <li>
+            <a href="">Products</a>
+          </li>
+          <li>
+            <a href="">Features</a>
+          </li>
+          <li>
+            <a href="">Pricing</a>
+          </li>
+          <li>
+            <a href="">Testimonials</a>
+          </li>
+          <li>
+            <a href="">Testimonials</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end flex gap-2 lg:gap-4">
+        <span className="flex gap-1 lg:gap-4 font-semibold">
+          <CiShoppingCart size={22} />
+          Login
+        </span>
+        <a className="btn text-sm text-white rounded-full bg-linear-to-r from-blue-500 to-purple-500  ">
+          Get Started
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
