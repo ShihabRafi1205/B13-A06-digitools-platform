4a8f6ad34522ae6337f257a1074e439938f42b55
ShihabRafi1205 <rafy191205@gmail.com>
2026-04-01 10:04:50 +0200

    added tooltip in banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import BannerImg from "../../assets/banner.png";
 import { IoPlayOutline } from "react-icons/io5";
-
+import imgTooltip from "../../assets/products/Group 5.png";
 const Banner = () => {
   return (
     <div className="hero mt-8 md:mb-8 lg:min-h-screen">
       <div className="hero-content flex lg:gap-24 flex-col lg:flex-row-reverse">
         <img src={BannerImg} className="max-w-sm rounded-lg w-full" />
         <div>
+          <div className="text-sm bg-[#E1E7FF] rounded-full p-1.5 px-3 flex inline-flex justify-center items-center gap-2 mb-5">
+            <img src={imgTooltip} alt="" />{" "}
+            <span className="bg-linear-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
+              New: AI-Powered Tools Available
+            </span>
+          </div>
           <h1 className="text-5xl font-bold">
             Supercharge Your <br /> Digital Workflow
           </h1>
